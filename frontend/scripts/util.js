6c45c0211c2efc7953687ce4a8deca04cdb175a5
Mark <123863080+marlef03@users.noreply.github.com>
2026-04-12 00:19:34 +0300

    [feat]: Изменение внешнего вида шапки сайта, начало работы над содержанием главной страницы

--- frontend/scripts/util.js
+++ frontend/scripts/util.js
@@ -1,8 +1,34 @@
-function adjust_height() {
-    let headerHeight = document.getElementById('header').offsetHeight;
-    document.getElementById('main').style.marginTop = headerHeight + 'px';
+function handle_header() {
+    console.log(window.scrollY);
+    const linkTexts = document.querySelectorAll('.link__text');
+    const headerLinks = document.querySelector('.header__links');
+    const titleHeight = document.querySelector('.header__title').offsetHeight;
+    if (window.scrollY >= titleHeight) {
+        linkTexts.forEach(text => {
+            void text.offsetWidth;
+            text.classList.add('closed');
+            void text.offsetWidth;
+        });
+        void headerLinks.offsetWidth;
+        headerLinks.classList.add('closed');
+        void headerLinks.offsetWidth;
+
+        document.querySelector('.header').style.paddingBottom = headerLinks.offsetHeight + 'px';
+    }
+    else {
+        linkTexts.forEach(text => {
+            void text.offsetWidth;
+            text.classList.remove('closed');
+            void text.offsetWidth;
+        });
+        void headerLinks.offsetWidth;
+        headerLinks.classList.remove('closed');
+        void headerLinks.offsetWidth;
+
+        document.querySelector('.header').style.paddingBottom = '0px';
+    }
 }
 
-window.addEventListener('resize', adjust_height);
+window.addEventListener('scroll', handle_header);
 
-adjust_height();
+handle_header();
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from russian-computer-history!")
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+import uvicorn
 
+app = FastAPI()
+
+
+app.mount('/frontend', StaticFiles(directory='frontend'), name='frontend')
+app.mount('/assets', StaticFiles(directory='assets'), name='assets')
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run('main:app', host='192.168.0.110', reload=True)
